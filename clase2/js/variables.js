c90545edf0127c61c50869012f33b018fd85f38a
marcos pinardi <exegeses@gmail.com>
2020-08-10 20:51:11 -0300

    escribir en HTML usando variables

--- clase2/js/variables.js
+++ clase2/js/variables.js
@@ -1,6 +1,9 @@
 // creamos un variable
-
     var numero = prompt('Ingrese un número');
-
+    var numero2 = prompt('Ingrese otro número');
 // ubicamos elemento dentro del DOM
-    document.getElementById('texto').innerText = numero;
+    var texto = document.getElementById('texto');
+
+// parseInt() sirve para convertir un string a número entero
+// parseFloat() sirve para convertir un string a número con decimales
+    texto.innerText = parseInt(numero) + parseInt(numero2);
