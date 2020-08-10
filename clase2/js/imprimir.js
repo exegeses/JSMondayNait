919f0999f641b55742275e89badd7f823be5876a
marcos pinardi <exegeses@gmail.com>
2020-08-10 20:02:47 -0300

    escribir en el HTML usando innerText

---
+++ clase2/js/imprimir.js
@@ -0,0 +1,5 @@
+// escribir en el HTML
+    document.write('saludos de Gabriela');
+
+// ubicar span dentro del DOM
+    document.getElementById('frase').innerText = 'Texto generado con javascript';
