e7e28a739c2b665cc094e129fa45a779985fd53e
marcos pinardi <exegeses@gmail.com>
2020-08-10 22:02:32 -0300

    mostrar ocultar

---
+++ clase2/js/mostrarOcultar.js
@@ -0,0 +1,18 @@
+// ubicar elementos dentro del DOM
+    var caja = document.getElementById('caja');
+
+// generar funciones
+    function ocultar()
+    {
+        // opacity = '0';
+        // visibility = 'hidden'
+        // display = 'none'
+        caja.style.display = 'none';
+    }
+    function mostrar()
+    {
+        // opacity = '1';
+        // visibility = 'visible'
+        // display = 'block'
+        caja.style.display = 'block';
+    }
