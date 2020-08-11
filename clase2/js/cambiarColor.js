117bfa8f367c7d96c4a0cc901b673788362b2400
marcos pinardi <exegeses@gmail.com>
2020-08-10 21:31:34 -0300

    ejercicio funciones

---
+++ clase2/js/cambiarColor.js
@@ -0,0 +1,19 @@
+// ubicar elemento dentro del DOM
+    var caja = document.getElementById('caja');
+
+// creamos funciones
+
+    function naranja()
+    {
+        caja.style.backgroundColor='#e0a87d';
+        caja.innerText = 'Naranja';
+        caja.style.color = '#813c02';
+    }
+
+    function verde()
+    {
+        caja.style.backgroundColor='#bde07d';
+        caja.innerText = 'Verde';
+        caja.style.color = '#025f10';
+    }
+
---
+++ clase2/notas.md
@@ -0,0 +1,7 @@
+# Variables
+> Una variable sirve para guardar un dato.
+
+# Funciones
+
+> ¿qué pasa cuando queremos guardar varias líneas de código?  
+> ahí recurriremos a las funciones
