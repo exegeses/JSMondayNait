d63493072bb171c3f6d37b56c2422a5826f6e544
marcos pinardi <exegeses@gmail.com>
2020-08-24 21:43:41 -0300

    innerText vs innerHTML

---
+++ clase3/js/innerHTML.js
@@ -0,0 +1,8 @@
+// ubicamos elemento dentro del DOM
+    var foto = document.getElementById('foto');
+
+
+    //usamos innerText para texto
+    //foto.innerText = '<img src="imagenes/ninja-coder.png">';
+
+    foto.innerHTML = '<img src="imagenes/ninja-coder.png">';
